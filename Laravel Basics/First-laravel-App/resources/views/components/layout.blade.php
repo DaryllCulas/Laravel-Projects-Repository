190c08c0c3f7ab01ee5d2643ec7d6a2e923d81b5
DaryllCulas <culasdaryll99@gmail.com>
2024-07-24 03:58:41 +0800

    Blade & Anonymous Components Added && Tailwind Configs Added

---
+++ Laravel Basics/First-laravel-App/resources/views/components/layout.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>{{ env('APP_NAME') }}</title>
+  @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body class="bg-slate-100 text-slate-900">
+    <header class="bg-slate-800 shadow-lg">
+        <nav>
+            <a href="#" class="nav-link">Home</a>
+                <div class="flex items-center gap-4">
+                    <a href="#" class="nav-link">Login</a>
+                    <a href="#" class="nav-link">Register</a>
+                </div>
+        </nav>
+    </header>
+    <main class="py-8 px-4 mx-auto max-w-screen-lg">
+        {{ $slot }}
+    </main>
+
+</body>
+</html>
---
+++ Laravel Basics/First-laravel-App/resources/views/welcome.blade.php
@@ -0,0 +1,10 @@
+
+    <x-layout>
+        <h1>Hello</h1>
+    </x-layout>
+
+    <x-demo></x-demo>
+
+
+
+
