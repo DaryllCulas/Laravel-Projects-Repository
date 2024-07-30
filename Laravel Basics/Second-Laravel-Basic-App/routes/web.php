33a596f2f61aaec846584289e0e9debf03014352
DaryllCulas <culasdaryll99@gmail.com>
2024-07-31 05:52:18 +0800

    Autoloading, Namespaces and Models

--- Laravel Basics/Second-Laravel-Basic-App/routes/web.php
+++ Laravel Basics/Second-Laravel-Basic-App/routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
-use Illuminate\Support\Arr;
+use App\Models\Job;
 use Illuminate\Support\Facades\Route;
+
+
+
 
 Route::get('/', function () {
     return view('home');
@@ -9,49 +12,11 @@ Route::get('/', function () {
 
 
 Route::get('/jobs', function () {
-    return view('jobs', [
-        'jobs' => [
-            [
-                'id' => 1,
-                'title' => 'Senior Laravel Developer',
-                'salary' => '$100, 000',
-            ],
-            [
-                'id' => 2,
-                'title' => 'Programmer',
-                'salary' => '$10, 000'
-            ],
-            [
-                'id' => 3,
-                'title' => 'Teacher',
-                'salary' => '$50, 000'
-            ]
-        ]
-    ]);
+    return view('jobs', ['jobs' => Job::jobItem()]);
 });
 
 Route::get('/jobs/{id}', function ($id) {
-    $jobs = [
-        [
-            'id' => 1,
-            'title' => 'Senior Laravel Developer',
-            'salary' => '$100, 000',
-        ],
-        [
-            'id' => 2,
-            'title' => 'Programmer',
-            'salary' => '$10, 000'
-        ],
-        [
-            'id' => 3,
-            'title' => 'Teacher',
-            'salary' => '$50, 000'
-        ]
-    ];
-
-    $job = Arr::first($jobs, function ($item) use ($id) {
-        return $item['id'] == $id;
-    });
+    $job = Job::find($id);
 
     return view('job', ['job' => $job]);
 });
