595cd25534f5d1bf0f88065ab1db69bad4522ec5
DaryllCulas <culasdaryll99@gmail.com>
2024-07-31 02:24:45 +0800

    Style currently active navigation link

--- Laravel Basics/Second-Laravel-Basic-App/resources/views/components/layout.blade.php
+++ Laravel Basics/Second-Laravel-Basic-App/resources/views/components/layout.blade.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
-<html class="h-full bg-gray-100" lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html class="h-full bg-gray-100">
 
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Home</title>
+    <title>Second Laravel App</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
@@ -30,13 +30,9 @@
                         <div class="hidden md:block">
                             <div class="ml-10 flex items-baseline space-x-4">
                                 <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                                <a href="/" class="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white"
-                                    aria-current="page">Home</a>
-                                <a href="/about"
-                                    class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">About</a>
-                                <a href="/contact"
-                                    class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Contacts</a>
-
+                                <x-nav-link href="/" :active="request()->is('/')"> Home </x-nav-link>
+                                <x-nav-link href="/about" :active="request()->is('about')"> About </x-nav-link>
+                                <x-nav-link href="/contact" :active="request()->is('contact')"> Contacts </x-nav-link>
                             </div>
                         </div>
                     </div>
