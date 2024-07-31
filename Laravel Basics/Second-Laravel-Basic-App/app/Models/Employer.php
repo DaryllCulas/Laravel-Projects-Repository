7e2bcea3e2200af11d0b1390b5c2656b8ce881a2
DaryllCulas <culasdaryll99@gmail.com>
2024-08-01 05:19:17 +0800

    Two-Key-Eloquent Relationship

--- Laravel Basics/Second-Laravel-Basic-App/app/Models/Employer.php
+++ Laravel Basics/Second-Laravel-Basic-App/app/Models/Employer.php
@@ -2,10 +2,17 @@
 
 namespace App\Models;
 
+use App\Models\Job;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Employer extends Model
 {
     use HasFactory;
+
+    public function jobs()
+    {
+        // One-to-Many relationship = This employer has many jobs
+        return $this->hasMany(Job::class);
+    }
 }
--- Laravel Basics/Second-Laravel-Basic-App/app/Models/Job.php
+++ Laravel Basics/Second-Laravel-Basic-App/app/Models/Job.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Models\Employer;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -13,4 +14,12 @@ class Job extends Model
     protected $table = 'job_listings';
 
     protected $fillable = ['title', 'salary'];
+
+
+    public function employer()
+    {
+
+        // One-to-One | One-to-Many | Relationship (belongsTo) = This job belongs to an employer
+        return $this->belongsTo(Employer::class);
+    }
 }
