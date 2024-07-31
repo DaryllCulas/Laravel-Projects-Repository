7fbc96c0ae5623ac180871aefaeaf87610174d76
DaryllCulas <culasdaryll99@gmail.com>
2024-08-01 02:56:50 +0800

    Model Factories

---
+++ Laravel Basics/Second-Laravel-Basic-App/database/factories/JobFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Employer;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Job>
+ */
+class JobFactory extends Factory
+{
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => $this->faker->jobTitle(),
+            'employer_id' => Employer::factory(),
+            'salary' => '$' . $this->faker->numberBetween(10000, 100000),
+        ];
+    }
+}
--- Laravel Basics/Second-Laravel-Basic-App/database/factories/UserFactory.php
+++ Laravel Basics/Second-Laravel-Basic-App/database/factories/UserFactory.php
@@ -24,7 +24,8 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
+            'first_name' => fake()->firstname(),
+            'last_name' => fake()->lastname(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
