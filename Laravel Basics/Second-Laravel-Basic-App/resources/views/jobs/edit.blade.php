12d95ba2b18c9493b29d43c2efafa4e047860c74
DaryllCulas <culasdaryll99@gmail.com>
2024-08-02 05:59:46 +0800

    Editing, Updating and Deleting a Resource

--- Laravel Basics/Second-Laravel-Basic-App/resources/views/jobs/edit.blade.php
+++ Laravel Basics/Second-Laravel-Basic-App/resources/views/jobs/edit.blade.php
@@ -1,15 +1,13 @@
 <x-layout>
     <x-slot:heading>
-        Create Job
+        Edit Job: {{ $job->title }}
     </x-slot:heading>
 
-    <form method="POST" action="/jobs">
+    <form method="POST" action="/jobs/{{ $job->id }}">
         @csrf
+        @method('PATCH')
         <div class="space-y-12">
             <div class="border-b border-gray-900/10 pb-12">
-                <h2 class="text-base font-semibold leading-7 text-gray-900">Create a New Job</h2>
-                <p class="mt-1 text-sm leading-6 text-gray-600">We just need a handful of details from you.</p>
-
                 <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
                     <div class="sm:col-span-4">
                         <label for="title" class="block text-sm font-medium leading-6 text-gray-900">Title</label>
@@ -19,7 +17,7 @@
 
                                 <input type="text" name="title" id="title"
                                     class="block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                                    placeholder="Shift Leader" required>
+                                    placeholder="Shift Leader" value="{{ $job->title }}" required>
                             </div>
                             @error('title')
                             <p class="text-xs text-red-500 font-semibold mt-1"> {{ $message }}</p>
@@ -35,7 +33,7 @@
 
                                 <input type="text" name="salary" id="salary"
                                     class="block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                                    placeholder="$50,000 Per year" required>
+                                    placeholder="$50,000 Per year" value="{{ $job->salary  }}" required>
                             </div>
                             @error('salary')
                             <p class="text-xs text-red-500 font-semibold mt-1"> {{ $message }}</p>
@@ -51,12 +49,24 @@
 
         </div>
 
-        <div class="mt-6 flex items-center justify-end gap-x-6">
-            <button type="button" class="text-sm font-semibold leading-6 text-gray-900">Cancel</button>
-            <button type="submit"
-                class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
+        <div class="mt-6 flex items-center justify-between gap-x-6">
+            <div class="flex items-center">
+                <button form="delete-form" class="text-red-500 text-sm font-bold">Delete</button>
+            </div>
+            <div class="flex items-center gap-x-6">
+                <a href="/jobs/{{ $job->id }}" type="button"
+                    class="text-sm font-semibold leading-6 text-gray-900">Cancel</a>
+                <div>
+                    <button type="submit"
+                        class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Update</button>
+                </div>
+            </div>
         </div>
     </form>
 
+    <form id="delete-form" action="/jobs/{{ $job->id }}" method="post" class="hidden">
+        @csrf
+        @method('DELETE')
+    </form>
 
 </x-layout>
--- Laravel Basics/Second-Laravel-Basic-App/routes/web.php
+++ Laravel Basics/Second-Laravel-Basic-App/routes/web.php
@@ -4,13 +4,13 @@ use App\Models\Job;
 use Illuminate\Support\Facades\Route;
 
 
-
-
+// It serves as home page
 Route::get('/', function () {
     return view('home');
 });
 
 
+// It serves as index page
 Route::get('/jobs', function () {
 
     // eager loading here to avoid N+1 queries and apply pagination
@@ -19,12 +19,22 @@ Route::get('/jobs', function () {
     return view('jobs.index', ['jobs' => $jobs]);
 });
 
+
+// It serves as create page
 Route::get('/jobs/create', function () {
     return view('jobs.create');
 });
 
 
+
+// It serves as store page
 Route::post('/jobs', function () {
+    // Validation
+    request()->validate([
+        'title' => ['required', 'min:5'],
+        'salary' => ['required']
+    ]);
+
 
     Job::create([
         'title' => request('title'),
@@ -35,12 +45,51 @@ Route::post('/jobs', function () {
     return redirect('/jobs');
 });
 
+
+// It serves as show page
 Route::get('/jobs/{id}', function ($id) {
     $job = Job::find($id);
 
     return view('jobs.show', ['job' => $job]);
 });
 
+// It serves as edit page
+Route::get('/jobs/{id}/edit', function ($id) {
+    $job = Job::find($id);
+
+    return view('jobs.edit', ['job' => $job]);
+});
+
+
+// It serves as update page
+Route::patch('/jobs/{id}', function ($id) {
+
+    request()->validate([
+        'title' => ['required', 'min:5'],
+        'salary' => ['required']
+    ]);
+
+
+    $job = Job::findOrFail($id);
+
+    $job->update([
+        'title' => request('title'),
+        'salary' => request('salary'),
+    ]);
+
+    return redirect('/jobs/' . $job->id);
+});
+
+
+// It serves as destroy page
+Route::delete('/jobs/{id}', function ($id) {
+
+    $job = Job::findOrFail($id)->delete();
+
+    return redirect('/jobs');
+});
+
+// It serves as contact page
 Route::get('/contact', function () {
     return view('contact');
 });
