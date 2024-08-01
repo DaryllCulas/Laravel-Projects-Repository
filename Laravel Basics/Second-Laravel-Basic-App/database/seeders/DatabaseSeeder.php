d67a941cd2fd1c7115c85e4357f93884eb728920
DaryllCulas <culasdaryll99@gmail.com>
2024-08-02 03:02:42 +0800

    Database seeders

--- Laravel Basics/Second-Laravel-Basic-App/database/seeders/DatabaseSeeder.php
+++ Laravel Basics/Second-Laravel-Basic-App/database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Database\Seeders\JobSeeder;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -16,8 +17,12 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'first_name' => 'Jisas',
+            'last_name' => 'fries',
+            'email' => 'jisasfries99@example.com',
         ]);
+
+
+        $this->call(JobSeeder::class);
     }
 }
