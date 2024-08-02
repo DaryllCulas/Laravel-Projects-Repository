c2dff6c9420db6e4196631ea1ae2ce84f72e45ed
DaryllCulas <culasdaryll99@gmail.com>
2024-08-03 01:35:49 +0800

    Make a login and Registration System from scratch Pt.1

--- Laravel Basics/Second-Laravel-Basic-App/routes/web.php
+++ Laravel Basics/Second-Laravel-Basic-App/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\JobController;
+use App\Http\Controllers\RegisteredUserController;
+use App\Http\Controllers\SessionController;
 use App\Models\Job;
 use Illuminate\Support\Facades\Route;
 
@@ -21,8 +23,12 @@ Route::delete('/jobs/{job}', [JobController::class, 'destroy']);
 Route::view('/contact', 'contact');
 
 
+// Auth routes
+Route::get('/register', [RegisteredUserController::class, 'create']);
+Route::post('/register', [RegisteredUserController::class, 'store']);
 
-
+Route::get('/login', [SessionController::class, 'create']);
+Route::post('/login', [SessionController::class, 'store']);
 
 //-------------------------Shorthand Routes------------------------------------
 
