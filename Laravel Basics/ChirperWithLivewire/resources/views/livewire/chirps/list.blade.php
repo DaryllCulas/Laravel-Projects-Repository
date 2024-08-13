957a925a014431ac7ec788ba40984e21474fe8f1
DaryllCulas <culasdaryll99@gmail.com>
2024-08-14 04:18:21 +0800

    Delete => Updating the component

--- Laravel Basics/ChirperWithLivewire/resources/views/livewire/chirps/list.blade.php
+++ Laravel Basics/ChirperWithLivewire/resources/views/livewire/chirps/list.blade.php
@@ -37,6 +37,15 @@ new class extends Component {
         $this->getChirps();
     }
 
+    public function delete(Chirp $chirp): void
+    {
+        $this->authorize('delete', $chirp);
+
+        $chirp->delete();
+
+        $this->getChirps();
+    }
+
 }; ?>
 
 <div class="mt-6 bg-white shadow sm:rounded-lg divide-y">
@@ -70,6 +79,10 @@ new class extends Component {
                     <x-slot name="content">
                         <x-dropdown-link wire:click="edit({{ $chirp->id }})">
                             {{ __('Edit') }}
+                        </x-dropdown-link>
+                        <x-dropdown-link wire:click="delete({{ $chirp->id }})"
+                            wire:confirm="Are you sure you want to delete this chirp?">
+                            {{ __('Delete') }}
                         </x-dropdown-link>
                     </x-slot>
                 </x-dropdown>
