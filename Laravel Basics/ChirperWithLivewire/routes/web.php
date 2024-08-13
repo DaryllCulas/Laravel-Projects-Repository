114696ca7b74453d086b14e398cc448ee2d31a9a
DaryllCulas <culasdaryll99@gmail.com>
2024-08-13 23:45:44 +0800

    ChirpsWithLivewire added => modify routing

---
+++ Laravel Basics/ChirperWithLivewire/routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use App\Http\Controllers\ChirpController;
+use Illuminate\Support\Facades\Route;
+
+Route::view('/', 'welcome');
+
+Route::get('chirps', [ChirpController::class])
+    ->middleware(['auth', 'verified'])
+    ->name('chirps');
+
+Route::view('dashboard', 'dashboard')
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
+
+Route::view('profile', 'profile')
+    ->middleware(['auth'])
+    ->name('profile');
+
+require __DIR__ . '/auth.php';
