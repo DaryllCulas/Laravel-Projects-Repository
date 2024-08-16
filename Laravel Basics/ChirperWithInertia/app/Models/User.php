453faf0e46917acef709a148b4f4a0d633ac85e2
DaryllCulas <culasdaryll99@gmail.com>
2024-08-17 03:28:29 +0800

    Creating a relationship

--- Laravel Basics/ChirperWithInertia/app/Models/User.php
+++ Laravel Basics/ChirperWithInertia/app/Models/User.php
@@ -3,7 +3,9 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Models\Chirp;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -43,5 +45,10 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function chirps(): HasMany
+    {
+        return $this->hasMany(Chirp::class);
     }
 }
