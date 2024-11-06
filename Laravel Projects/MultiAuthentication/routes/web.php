5de909d2bbf7ad188e10678dd0e37c81b2118e2d
DaryllCulas <culasdaryll99@gmail.com>
2024-11-06 22:26:03 +0800

    Create Migration/Model, Routes, Update Laravel UI, UserAccess middleware and Routes

--- Laravel Projects/MultiAuthentication/routes/web.php
+++ Laravel Projects/MultiAuthentication/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -11,10 +12,35 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+/** Normal Users Route List */
+
+Route::middleware(['auth', 'user-access:user'])->group(function () {
+    Route::get('/home', [HomeController::class, 'index'])->name('home');
+});
+
+/** Admin Route List */
+Route::middleware(['auth', 'user-access:admin'])->group(function () {
+    Route::get('admin/home', [HomeController::class, 'adminHome'])->name('admin.home');
+});
+
+/** Manager Route List */
+Route::middleware(['auth', 'user-access:manager'])->group(function () {
+    Route::get('/manager/home', [HomeController::class, 'managerHome'])->name('manager.home');
+});
+
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
