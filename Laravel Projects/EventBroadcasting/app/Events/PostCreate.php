b471010124a4541b3acf3f5d7a94d881157fb319
DaryllCulas <culasdaryll99@gmail.com>
2024-11-07 02:46:05 +0800

    Create Post Event

---
+++ Laravel Projects/EventBroadcasting/app/Events/PostCreate.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class PostCreate implements ShouldBroadcastNow
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $post;
+
+    /**
+     * Create a new event instance.
+     */
+    public function __construct($post)
+    {
+        $this->post = $post;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return array<int, \Illuminate\Broadcasting\Channel>
+     */
+    public function broadcastOn(): array
+    {
+        return [
+            new PrivateChannel('posts'),
+        ];
+    }
+
+
+    public function broadCastAs()
+    {
+        return 'Create';
+    }
+
+    public function broadCastWith(): array
+    {
+        return [
+            'message' => "[{$this->post->created_at}] New Post Received with title: '{$this->post->title}'."
+        ];
+    }
+}
