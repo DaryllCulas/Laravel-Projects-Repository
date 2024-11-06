63556e886a5fe0c9177bf87b33e1bb52102c36a1
DaryllCulas <culasdaryll99@gmail.com>
2024-11-06 21:49:03 +0800

    MultiAuthentication

---
+++ Laravel Projects/MultiAuthentication/database/migrations/2024_11_06_133114_add_type_to_users_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            /**
+             * User: 0 => User, 1 => Admin, 2=> Manager
+             *
+             */
+            $table->tinyInteger('type')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+};
