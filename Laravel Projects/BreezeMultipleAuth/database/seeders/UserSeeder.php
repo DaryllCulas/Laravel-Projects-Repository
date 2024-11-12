33cc7f844b6d6d6d68602425cac4c40c469baec2
DaryllCulas <culasdaryll99@gmail.com>
2024-11-12 09:58:39 +0800

    Create a UserSeeder and Seeding the database

---
+++ Laravel Projects/BreezeMultipleAuth/database/seeders/UserSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('password'),
+            'role' => 'admin'
+        ]);
+
+        User::create([
+            'name' => 'Agent',
+            'email' => 'agent@gmail.com',
+            'password' => Hash::make('password'),
+            'role' => 'agent'
+        ]);
+
+
+        User::create([
+            'name' => 'User',
+            'email' => 'user@gmail.com',
+            'password' => Hash::make('password'),
+            'role' => 'user'
+        ]);
+    }
+}
