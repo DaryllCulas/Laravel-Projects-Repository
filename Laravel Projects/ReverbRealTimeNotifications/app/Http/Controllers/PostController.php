e061dce39b6085e70021e82bf565581d06c2cb63
DaryllCulas <culasdaryll99@gmail.com>
2024-11-16 02:33:03 +0800

    Create Controller

---
+++ Laravel Projects/ReverbRealTimeNotifications/app/Http/Controllers/PostController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Events\PostCreate;
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function index(Request $request)
+    {
+        $posts = Post::get();
+
+        return view('posts', ['posts' => $posts]);
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'title' => 'required',
+            'body' => 'required',
+        ]);
+
+        $post = Post::create([
+            'user_id' => auth()->id(),
+            'title' => $request->title,
+            'body' => $request->body,
+        ]);
+
+        event(new PostCreate($post));
+
+        return back()->with('success', 'Post Created Successfully');
+    }
+}
