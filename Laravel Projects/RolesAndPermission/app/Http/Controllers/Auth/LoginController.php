dc816b58db92d2dd954813c3d5bbd5989d498bcc
DaryllCulas <culasdaryll99@gmail.com>
2024-11-30 23:45:03 +0800

    Add Logout method

--- Laravel Projects/RolesAndPermission/app/Http/Controllers/Auth/LoginController.php
+++ Laravel Projects/RolesAndPermission/app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -36,5 +37,13 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
         $this->middleware('auth')->only('logout');
+    }
+
+    protected function Logout(Request $request)
+    {
+        $this->guard()->logout();
+        $request->session()->flush();
+        $request->session()->regenerate();
+        return redirect('/login');
     }
 }
