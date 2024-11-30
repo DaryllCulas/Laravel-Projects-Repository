deb163998ff077a2e42ac5bd0170d5d996c9a3d5
DaryllCulas <culasdaryll99@gmail.com>
2024-12-01 02:15:53 +0800

    Create a Product Model with Migration, Resource Controller, and Form Requests

--- Laravel Projects/RolesAndPermission/app/Http/Controllers/ProductController.php
+++ Laravel Projects/RolesAndPermission/app/Http/Controllers/ProductController.php
@@ -2,9 +2,87 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\StoreProductRequest;
+use App\Http\Requests\UpdateProductRequest;
+use App\Models\Product;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\View\View;
 
 class ProductController extends Controller
 {
-    //
+    public function __construct()
+    {
+        $this->middleware('auth');
+        $this->middleware('permission:view-product|create-product|edit-product|delete-product', ['only' => ['index', 'show']]);
+        $this->middleware('permission:create-product', ['only' => ['create', 'store']]);
+        $this->middleware('permission:edit-product', ['only' => ['edit', 'update']]);
+        $this->middleware('permission:delete-product', ['only' => ['destroy']]);
+    }
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(): View
+    {
+        return view('products.index', [
+            'products' => Product::latest()->paginate(3)
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create(): View
+    {
+        return view('products.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreProductRequest $request): RedirectResponse
+    {
+        Product::create($request->all());
+        return redirect()->route('products.index')
+            ->withSuccess('New Product is added successfully');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Product $product): View
+    {
+        return view('products.show', [
+            'products' => $product
+        ]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Product $product)
+    {
+        return view('products.edit', [
+            'product' => $product
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateProductRequest $request, Product $product)
+    {
+        $product->update($request->all());
+        return redirect()->back()
+            ->withSuccess('Product is updated successfully');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Product $product)
+    {
+        $product->delete();
+        return redirect()->route('products.index')
+            ->withSuccess('Product is deleted successfully');
+    }
 }
