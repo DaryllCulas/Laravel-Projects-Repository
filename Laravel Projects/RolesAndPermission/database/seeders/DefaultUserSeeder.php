f3979cc4d63f3747befb81c468ef33ea7547faad
DaryllCulas <culasdaryll99@gmail.com>
2024-12-01 01:28:24 +0800

    Create Permission, Role, and Default User Seeder

---
+++ Laravel Projects/RolesAndPermission/database/seeders/DefaultUserSeeder.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class DefaultUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $superAdmin = User::create([
+            'name' => 'superadmin',
+            'email' => 'superadmin@gmail.com',
+            'password' => Hash::make('password')
+        ]);
+
+        $superAdmin->assignRole('Super Admin');
+
+        $admin = User::create([
+            'name' => 'admin',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('password')
+        ]);
+
+        $admin->assigneRole('Admin');
+
+        $productManager = User::create([
+            'name' => 'productmanager',
+            'email' => 'productmanager@gmail.com',
+            'password' => Hash::make('password')
+        ]);
+
+        $productManager->assigneRole('Product Manager');
+
+        $user = User::create([
+            'name' => 'user',
+            'email' => 'user@gmail.com',
+            'password' => Hash::make('password')
+        ]);
+
+        $user->assignRole('User');
+    }
+}
