d059e7aa078f9871180fa78d61089295cb2bc04c
DaryllCulas <culasdaryll99@gmail.com>
2024-12-01 00:00:56 +0800

    Setup Laravel Spatie & Define SuperAdin

--- Laravel Projects/RolesAndPermission/app/Providers/AppServiceProvider.php
+++ Laravel Projects/RolesAndPermission/app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Gate::before(function ($user, $ability) {
+            return $user->hasRole('Super Admin') ? true : null;
+        });
+
+        Paginator::useBootstrapFive();
     }
 }
