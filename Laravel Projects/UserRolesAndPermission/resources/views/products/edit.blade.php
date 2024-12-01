9c82ec3968ef059f6a218a99f59046316dfe930c
DaryllCulas <culasdaryll99@gmail.com>
2024-12-01 18:58:50 +0800

    Sample for User Roles and Permissions only

---
+++ Laravel Projects/UserRolesAndPermission/resources/views/products/edit.blade.php
@@ -0,0 +1,53 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="row">
+    <div class="col-lg-12 margin-tb">
+        <div class="pull-left">
+            <h2>Edit Product</h2>
+        </div>
+        <div class="pull-right">
+            <a class="mb-2 btn btn-primary btn-sm" href="{{ route('products.index') }}"><i class="fa fa-arrow-left"></i>
+                Back</a>
+        </div>
+    </div>
+</div>
+
+@if ($errors->any())
+<div class="alert alert-danger">
+    <strong>Whoops!</strong> There were some problems with your input.<br><br>
+    <ul>
+        @foreach ($errors->all() as $error)
+        <li>{{ $error }}</li>
+        @endforeach
+    </ul>
+</div>
+@endif
+
+<form action="{{ route('products.update',$product->id) }}" method="POST">
+    @csrf
+    @method('PUT')
+
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Name:</strong>
+                <input type="text" name="name" value="{{ $product->name }}" class="form-control" placeholder="Name">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Detail:</strong>
+                <textarea class="form-control" style="height:150px" name="detail"
+                    placeholder="Detail">{{ $product->detail }}</textarea>
+            </div>
+        </div>
+        <div class="text-center col-xs-12 col-sm-12 col-md-12">
+            <button type="submit" class="mt-2 mb-2 btn btn-primary btn-sm"><i class="fa-solid fa-floppy-disk"></i>
+                Submit</button>
+        </div>
+    </div>
+</form>
+
+<p class="text-center text-primary"><small>Tutorial by ItSolutionStuff.com</small></p>
+@endsection
---
+++ Laravel Projects/UserRolesAndPermission/routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\RoleController;
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::group(['middleware' => ['auth']], function () {
+    Route::resource('roles', RoleController::class);
+    Route::resource('users', UserController::class);
+    Route::resource('products', ProductController::class);
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__ . '/auth.php';
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
